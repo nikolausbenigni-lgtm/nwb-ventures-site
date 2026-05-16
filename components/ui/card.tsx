c257aae625121bb9c6a12fa03321f438262607f9
nikolausbenigni-lgtm <nikolaus.benigni@gmail.com>
2026-05-16 12:54:45 -0400

    Fix import path for utility functions in card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,4 +1,4 @@
 import * as React from "react";
-import { cn } from "@/lib/utils";
+import { cn } from "../../lib/utils";
 export function Card({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) { return <div className={cn("rounded-xl border", className)} {...props} />; }
 export function CardContent({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) { return <div className={cn("p-6", className)} {...props} />; }
